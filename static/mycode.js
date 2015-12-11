73b430fb6b7f5fe28dd359c1ae98f183da8e8986
Daisy <dbarbanel555@gmail.com>
2015-12-10 21:19:35 -0500

    added ajax and jquery stuff

--- static/mycode.js
+++ static/mycode.js
@@ -1,8 +1,39 @@
-var updateHunger = function updateHunger() {
-    var h = $("#hunger");
-    console.log(h);
-    
+
+
+var updateHunger = function updateHunger(n) {
+    newPercent = getHunger() + n + '%';
+    $("#hunger")[0].style.width = newPercent;
+    $("#hunger").text(newPercent);    
+}
+
+var getHunger = function getHunger(){
+    var h = $("#hunger")[0].style.width;
+    return parseFloat(h);
+}
+
+var temp = function temp(location){
+    $('#loc').text(location);
+    $.get("/temp", {city:location}, function(d){
+	      $("#temp").text(d + " " + decodeURI('%C2%B0') + "F")
+        feels(d);});
 }
 
 
-$("#hunger").attr("style", "width:" + (str)percentage);
+var feels = function feels(temp){
+    h = getHunger();
+    if (h < 50){
+        if (temp < 68){
+            $("#feels").text("hungry and cold");
+        } else {
+            $("#feels").text("hungry");
+        }
+    } else if (temp < 68){
+        $("#feels").text("cold");
+    } else {
+        $("#feels").text("happy");
+    }
+}
+
+temp("New York");	
+console.log(getHunger());
+updateHunger(5)
