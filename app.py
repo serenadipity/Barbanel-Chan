ca82959845763fe1ce7527f1a3e690f710aa3163
Daisy <dbarbanel555@gmail.com>
2015-12-10 21:20:34 -0500

    eddited html and added app route

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, redirect, request, session, url_for
-import urllib2, json
+import weather
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template("home.html")
+        
+	return render_template("home.html", name = "Name")
+
+@app.route("/temp")
+def temp():
+        if request.method == "GET":
+                city = request.args.get("city")
+                temp =  weather.get_temp(city)
+                return temp
+        return "no"
 
 if __name__ == "__main__":
    app.debug = True
